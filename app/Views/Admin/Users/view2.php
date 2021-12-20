1b269893b6d7c74644c4abaeca24c3775c657671
Patrick Karungari <pkarungari@gmail.com>
2021-12-20 14:23:54 -0500

    fix

--- app/Views/Admin/Users/view2.php
+++ app/Views/Admin/Users/view2.php
@@ -174,72 +174,7 @@ $_deposits['data']=$deposits;
     </div>
     <!-- User Card & Plan Ends -->
 
-    <!-- User Timeline & Permissions Starts -->
-    <div class=" row ">
-        <!-- information starts -->
-        <div class=" mx-1 w-100">
-            <div class="card">
-                <div class="card-header">
-                    <h4 class="card-title mb-2">User Timeline</h4>
-                </div>
-                <div class="card-body">
-                    <ul class="timeline">
-                        <li class="timeline-item">
-                            <span class="timeline-point timeline-point-indicator"></span>
-                            <div class="timeline-event">
-                                <div class="d-flex justify-content-between flex-sm-row flex-column mb-sm-0 mb-1">
-                                    <h6>12 Invoices have been paid</h6>
-                                    <span class="timeline-event-time">12 min ago</span>
-                                </div>
-                                <p>Invoices have been paid to the company.</p>
-                                <div class="media align-items-center">
-                                    <img class="mr-1" src="../../../assets/images/icons/file-icons/pdf.png"
-                                        alt="invoice" height="23" />
-                                    <div class="media-body">invoice.pdf</div>
-                                </div>
-                            </div>
-                        </li>
-                        <li class="timeline-item">
-                            <span class="timeline-point timeline-point-warning timeline-point-indicator"></span>
-                            <div class="timeline-event">
-                                <div class="d-flex justify-content-between flex-sm-row flex-column mb-sm-0 mb-1">
-                                    <h6>Client Meeting</h6>
-                                    <span class="timeline-event-time">45 min ago</span>
-                                </div>
-                                <p>Project meeting with john @10:15am.</p>
-                                <div class="media align-items-center">
-                                    <div class="avatar">
-                                        <img src="../../../assets/images/avatars/12-small.png" alt="avatar" height="38"
-                                            width="38" />
-                                    </div>
-                                    <div class="media-body ml-50">
-                                        <h6 class="mb-0">John Doe (Client)</h6>
-                                        <span>CEO of Infibeam</span>
-                                    </div>
-                                </div>
-                            </div>
-                        </li>
-                        <li class="timeline-item">
-                            <span class="timeline-point timeline-point-info timeline-point-indicator"></span>
-                            <div class="timeline-event">
-                                <div class="d-flex justify-content-between flex-sm-row flex-column mb-sm-0 mb-1">
-                                    <h6>Create a new project for client</h6>
-                                    <span class="timeline-event-time">2 days ago</span>
-                                </div>
-                                <p class="mb-0">Add files to new design folder</p>
-                            </div>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-        <!-- information Ends -->
 
-        <!-- User Permissions Starts -->
-
-        <!-- User Permissions Ends -->
-    </div>
-    <!-- User Timeline & Permissions Ends -->
 
     <!-- User Invoice Starts-->
     <div class="row invoice-list-wrapper">
@@ -270,7 +205,33 @@ $_deposits['data']=$deposits;
     <!-- /User Invoice Ends-->
 
 
-
+    <!-- User Invoice Starts-->
+    <div class="row invoice-list-wrapper">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-header">
+                    <h4 class="card-title mb-2">User Transactions</h4>
+                </div>
+                <div class="card-datatable table-responsive pb-1">
+                    <table class="investments-list-table table">
+                        <thead>
+                            <tr>
+                                <th></th>
+                                <th>#</th>
+                                <th>Amount</th>
+                                <th class="text-truncate">Date</th>
+                                <th>Type</th>
+                                <th>Status</th>
+                                <th class="cell-fit">Description</th>
+                                <th class="cell-fit">Actions</th>
+                            </tr>
+                        </thead>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- /User Invoice Ends-->
 
 
     <script>
--- assets/js/scripts/pages/app-user-view.js
+++ assets/js/scripts/pages/app-user-view.js
@@ -17,7 +17,7 @@ $(function () {
             1: { title: 'Pending', class: 'badge-light-info' },
             2: { title: 'Failed', class: 'badge-light-danger' }
         },
-    dtWithdrawsTable = $('.withdraw-list-table'),
+    dtInvestmentsTable = $('.investments-list-table'),
     assetPath = '../../../assets/',
     invoicePreview = 'app-invoice-preview.html',
     invoiceAdd = 'app-invoice-add.html',
@@ -314,5 +314,286 @@ $(function () {
       }
     });
   }
+
+if (dtInvestmentsTable.length) {
+    var dtInvestments = dtInvestmentsTable.DataTable({
+      "ajax": {
+        "url": "../gtrx",
+        "type": "POST",
+        "data": idObject
+      },
+      "language": {
+        "emptyTable": "User has not made any Transactions yet"
+      },
+      headers: { 'X-Requested-With': 'XMLHttpRequest' }, // JSON file to add data
+      autoWidth: false,
+      columns: [
+        // columns according to JSON
+        { data: 'id' },
+        { data: 'trx' },
+        { data: 'amount' },
+        { data: 'date' },
+        { data: 'type' },
+        { data: 'status' },
+        { data: 'description' },
+        { data: '' }
+      ],
+      columnDefs: [
+        {
+          // For Responsive
+          className: 'control',
+         
+          targets: 0
+        },
+        {
+        // Invoice ID
+          targets: 1,
+          responsivePriority: 2,
+          width: '24px',
+          render: function (data, type, full, meta) {
+             //console.log(data);
+            var $invoiceId = full['trx'];
+            // Creates full output for row
+            var $rowOutput = '<a class="font-weight-bold" href="' + invoicePreview + '"> #' + $invoiceId + '</a>';
+            return $rowOutput;
+          }
+        },
+       
+       
+        {
+          // Total Invoice Amount
+          targets: 2,
+          responsivePriority: 3,
+          width: '24px',
+          render: function (data, type, full, meta) {
+            var $total = full['amount'];
+            return '<span class="d-none">' + $total + '</span>$' + $total;
+          }
+        },
+        {
+          // Due Date
+          targets: 3,
+          width: '76px',
+          render: function (data, type, full, meta) {
+            var $dueDate = new Date(full['date']);
+            // Creates full output for row
+            var $rowOutput =
+              '<span class="d-none">' +
+              moment($dueDate).format('YYYYMMDD') +
+              '</span>' +
+              moment($dueDate).format('DD MMM YYYY');
+            $dueDate;
+            return $rowOutput;
+          }
+        },
+        {
+          //Invoice Type
+          targets: 4,
+          width: '24px',
+          render: function (data, type, full, meta) {
+            var $type = full['type'];
+            return '  <option class="text-capitalize"> <span class="d-none" >' + $type + '</span></option> ';
+          }
+        },
+         {
+          // Invoice status
+          targets: 5,
+          width: '130px',
+          render: function (data, type, full, meta) {
+            var $invoiceStatus = full['status'],
+              roleObj = {                
+                completed: { class: 'bg-light-success', icon: 'check-circle', title: 'Completed' },                
+                pending: { class: 'bg-light-info', icon: 'refresh-ccw', title: 'Pending' },
+                failed: { class: 'bg-light-danger', icon: 'info', title: 'Failed' }
+              };
+           
+            return (
+
+              '<div> <div class="avatar avatar-status ' +
+              roleObj[$invoiceStatus].class +
+              '">' +
+              '<span class="avatar-content">' +
+              feather.icons[roleObj[$invoiceStatus].icon].toSvg({ class: 'avatar-icon' }) +
+              '</span> </div>' +
+              '<span data-search =' + roleObj[$invoiceStatus].title + ' class="badge badge-pill ml-1 ' + roleObj[$invoiceStatus].class + '" text-uppercase>' + roleObj[$invoiceStatus].title + '</span>' +
+              ' </div>'
+    
+            );
+          }
+        },
+        {
+          // Invoice Description
+          targets: 6,
+          width: '180px',
+          render: function (data, type, full, meta) {
+            var $total = full['description'];
+            return '<div class="col-2 text-truncate"> <span class="d-none text-truncate">' + $total + '</span> </div>'+ $total ;
+          }
+        },
+       
+        {
+          // Actions
+          targets: 7,
+          title: 'Actions',
+          width: '76px',
+          orderable: false,
+          render: function (data, type, full, meta) {
+            return (
+              '<div class="d-flex align-items-center col-actions">' +
+              '<a class="mr-1" href="javascript:void(0);" data-toggle="tooltip" data-placement="top" title="Send Mail">' +
+              feather.icons['send'].toSvg({ class: 'font-medium-1' }) +
+              '</a>' +
+              '<a class="mr-1" href="' +
+              invoicePreview +
+              '" data-toggle="tooltip" data-placement="top" title="Preview Invoice">' +
+              feather.icons['eye'].toSvg({ class: 'font-medium-1' }) +
+              '</a>' +
+              '<div class="dropdown">' +
+              '<a class="btn btn-sm btn-icon px-0" data-toggle="dropdown">' +
+              feather.icons['more-vertical'].toSvg({ class: 'font-medium-1' }) +
+              '</a>' +
+              '<div class="dropdown-menu dropdown-menu-right">' +
+              '<a href="javascript:void(0);" class="dropdown-item">' +
+              feather.icons['download'].toSvg({ class: 'font-small-4 mr-50' }) +
+              'Download</a>' +
+              '<a href="' +
+              invoiceEdit +
+              '" class="dropdown-item">' +
+              feather.icons['edit'].toSvg({ class: 'font-small-4 mr-50' }) +
+              'Edit</a>' +
+              '<a href="javascript:void(0);" class="dropdown-item">' +
+              feather.icons['trash'].toSvg({ class: 'font-small-4 mr-50' }) +
+              'Delete</a>' +
+              '<a href="javascript:void(0);" class="dropdown-item">' +
+              feather.icons['copy'].toSvg({ class: 'font-small-4 mr-50' }) +
+              'Duplicate</a>' +
+              '</div>' +
+              '</div>' +
+              '</div>'
+            );
+          }
+        }
+      ],
+      order: [[1, 'desc']],
+      dom:
+        '<"row d-flex justify-content-between align-items-center m-1"' +        
+        '<"d-flex justify-content-between align-items-center mx-50 row pt-0 pb-2"f<"invoice_status ml-2"><"invoice_type ml-2">>' +
+        '<"col-lg-6 d-flex align-items-center"l<"dt-action-buttons text-xl-right text-lg-left text-md-right text-left "B>>' +
+        '>t' +
+        '<"d-flex justify-content-between mx-2 row"' +
+        '<"col-sm-12 col-md-6"i>' +
+        '<"col-sm-12 col-md-6"p>' +
+        '>',
+      language: {
+        sLengthMenu: 'Show _MENU_',
+        search: 'Search',
+        searchPlaceholder: 'Search Invoice',
+        paginate: {
+          // remove previous & next text from pagination
+          previous: '&nbsp;',
+          next: '&nbsp;'
+        }
+      },
+      // Buttons with Dropdown
+      buttons: [
+        {
+          text: 'Add Record',
+          className: 'btn btn-primary btn-add-record ml-2',
+          attr: {
+            'disabled': true
+            },
+          action: function (e, dt, button, config) {
+            window.location = invoiceAdd;
+          },
+          init: function (api, node, config) {
+            $(node).removeClass('btn-secondary');
+          }
+        }
+      ],
+      // For responsive popup
+      responsive: {
+        details: {
+          display: $.fn.dataTable.Responsive.display.modal({
+            header: function (row) {
+              var data = row.data();
+              return 'Details of ' + data['client_name'];
+            }
+          }),
+          type: 'column',
+          renderer: $.fn.dataTable.Responsive.renderer.tableAll({
+            tableClass: 'table',
+            columnDefs: [
+              {
+                targets: 2,
+                visible: false
+              },
+              {
+                targets: 3,
+                visible: false
+              }
+            ]
+          })
+        }
+      },
+      initComplete: function () {
+        // $('.dataTables_filter').find('.form-control-sm').removeClass('form-control-sm');
+        // $('.dataTables_length .custom-select').removeClass('custom-select-sm').removeClass('form-control-sm');
+        $(document).find('[data-toggle="tooltip"]').tooltip();
+        // Adding role filter once table initialized
+        this.api()
+          .columns(5)
+          
+          .every(function () {
+            var column = this;
+            var select = $(
+              '<select id="IvestmentStatus" class="form-control ml-50 text-capitalize"><option value=""> Select Investment Status </option></select>'
+            )
+              .appendTo('.invoice_status')
+              .on('change', function () {
+                var val = $.fn.dataTable.util.escapeRegex($(this).val());
+                column.search(val, true, false).draw();
+              });
+
+            column
+              .data()
+              .unique()
+              .sort()
+              .each(function (d, j) {
+                console.log( d );
+                select.append('<option value="' + d + '" class="text-capitalize">' + d + '</option>');
+              });
+          });
+        this.api()
+          .columns(4)
+          
+          .every(function () {
+            var column = this;
+            var select = $(
+              '<select id="IvestmentType" class="form-control ml-50 text-capitalize"><option value=""> Select Ivestment Type </option></select>'
+            )
+              .appendTo('.invoice_type')
+              .on('change', function () {
+                var val = $.fn.dataTable.util.escapeRegex($(this).val());
+                column.search(val, true, false).draw();
+              });
+
+            column
+              .data()
+              .unique()
+              .sort()
+              .each(function (d, j) {
+                console.log( d );
+                select.append('<option value="' + d + '" class="text-capitalize">' + d + '</option>');
+              });
+          });
+      },
+      
+      drawCallback: function () {
+        $(document).find('[data-toggle="tooltip"]').tooltip();
+      }
+    });
+  }
+
+
 
 });
