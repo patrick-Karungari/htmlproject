40f8d5d5705f6f380524d4b96ec09514f569aa5f
Patrick Karungari <pkarungari@gmail.com>
2021-12-25 17:51:42 -0500

    more work on investment page

--- assets/js/scripts/forms/pickers/form-pickers.js
+++ assets/js/scripts/forms/pickers/form-pickers.js
@@ -8,45 +8,18 @@
 ==========================================================================================*/
 (function (window, document, $) {
   'use strict';
+  var today = new Date();
+  const month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+  var dd = String(today.getDate()).padStart(2, '0');
+  var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+  var mmm = month[today.getMonth()]; //January is 0!
+  var yyyy = today.getFullYear();
 
+  var todayDate = yyyy + '-' + mm + '-' + dd;
+ 
   /*******  Flatpickr  *****/
-  var basicPickr = $('.flatpickr-basic'),
-    timePickr = $('.flatpickr-time'),
-    dateTimePickr = $('.flatpickr-date-time'),
-    multiPickr = $('.flatpickr-multiple'),
-    rangePickr = $('.flatpickr-range'),
-    humanFriendlyPickr = $('.flatpickr-human-friendly'),
-    disabledRangePickr = $('.flatpickr-disabled-range'),
-    inlineRangePickr = $('.flatpickr-inline');
+  var rangePickr = $('.flatpickr-range');
 
-  // Default
-  if (basicPickr.length) {
-    basicPickr.flatpickr();
-  }
-
-  // Time
-  if (timePickr.length) {
-    timePickr.flatpickr({
-      enableTime: true,
-      noCalendar: true
-    });
-  }
-
-  // Date & TIme
-  if (dateTimePickr.length) {
-    dateTimePickr.flatpickr({
-      enableTime: true
-    });
-  }
-
-  // Multiple Dates
-  if (multiPickr.length) {
-    multiPickr.flatpickr({
-      weekNumbers: true,
-      mode: 'multiple',
-      minDate: 'today'
-    });
-  }
 
 // Range
   if (rangePickr.length) {
@@ -58,185 +31,58 @@
         var dateStart = instance.formatDate(selectedDates[0], "Y-m-d");
         var dateEnd = instance.formatDate(selectedDates[1], "Y-m-d");
 
-      $.ajax({
-          url: "investments/getTotalInvestment",
-          data: {"start": dateStart, "end": dateEnd},
-          type: 'POST',
-        success: function (resp) {
-          if (resp != null) {
-            document.getElementById("subheading").innerHTML = "Ksh " + Intl.NumberFormat('en-US').format(resp);
-            alert('Success');
-            }
-           
-          },
-          error: function(e) {
-             alert('Error: '+e);
-          }  
-      });
+        $.ajax({
+            url: "investments/getTotalInvestment",
+            data: {"start": dateStart, "end": dateEnd},
+            type: 'GET',
+            success: function (resp) {
+              var Start = instance.formatDate(selectedDates[0], "M, d Y");
+              var End = instance.formatDate(selectedDates[1], "M, d Y");
+              if (resp != null) {
+                if (dateEnd == dateStart) {
+                  document.getElementById("heading").innerHTML = "Payouts for  " + Start;
+                } else {
+                  document.getElementById("heading").innerHTML = "Payouts for  " + Start + " to "+ End;
+                }
+              // document.getElementById("heading").innerHTML = "Payouts for  " + Intl.NumberFormat('en-US').format(resp);
+                document.getElementById("subheading").innerHTML = "Ksh " + Intl.NumberFormat('en-US').format(resp);
+                //alert('Success');
+              // rangePickr.flatpickr.close();
+              }            
+            },
+            error: function(e) {
+              alert('Error: '+e);
+            }  
+        });
       }
     });
    
   }
 
-
-
-  // Human Friendly
-  if (humanFriendlyPickr.length) {
-    humanFriendlyPickr.flatpickr({
-      altInput: true,
-      altFormat: 'F j, Y',
-      dateFormat: 'Y-m-d'
-    });
-  }
-
-  // Disabled Range
-  if (disabledRangePickr.length) {
-    disabledRangePickr.flatpickr({
-      dateFormat: 'Y-m-d',
-      disable: [
-        {
-          from: new Date().fp_incr(2),
-          to: new Date().fp_incr(7)
-        }
-      ]
-    });
-  }
-
-  // Inline
-  if (inlineRangePickr.length) {
-    inlineRangePickr.flatpickr({
-      inline: true
-    });
-  }
-  /*******  Pick-a-date Picker  *****/
-  // Basic date
-  $('.pickadate').pickadate();
-
-  // Format Date Picker
-  $('.format-picker').pickadate({
-    format: 'mmmm, d, yyyy'
+  // Default 
+  $.ajax({
+            url: "investments/getTotalInvestment",
+            data: {"start": todayDate, "end": todayDate},
+            type: 'GET',
+            success: function (resp) {
+           
+              if (resp != null) {
+                document.getElementById("heading").innerHTML = "Payouts for  " + mmm
+                  + ", " + dd + " " + yyyy;
+                document.getElementById("subheading").innerHTML = "Ksh " + Intl.NumberFormat('en-US').format(resp);
+               
+              }            
+            },
+            error: function(e) {
+              alert('Error: '+e.getMessage());
+            }  
   });
 
-  // Date limits
-  $('.pickadate-limits').pickadate({
-    min: [2019, 3, 20],
-    max: [2019, 5, 28]
-  });
 
-  // Disabled Dates & Weeks
-
-  $('.pickadate-disable').pickadate({
-    disable: [1, [2019, 3, 6], [2019, 3, 20]]
-  });
-
-  // Picker Translations
-  $('.pickadate-translations').pickadate({
-    formatSubmit: 'dd/mm/yyyy',
-    monthsFull: [
-      'Janvier',
-      'Février',
-      'Mars',
-      'Avril',
-      'Mai',
-      'Juin',
-      'Juillet',
-      'Août',
-      'Septembre',
-      'Octobre',
-      'Novembre',
-      'Décembre'
-    ],
-    monthsShort: ['Jan', 'Fev', 'Mar', 'Avr', 'Mai', 'Juin', 'Juil', 'Aou', 'Sep', 'Oct', 'Nov', 'Dec'],
-    weekdaysShort: ['Dim', 'Lun', 'Mar', 'Mer', 'Jeu', 'Ven', 'Sam'],
-    today: "aujourd'hui",
-    clear: 'clair',
-    close: 'Fermer'
-  });
-
-  // Month Select Picker
-  $('.pickadate-months').pickadate({
-    selectYears: false,
-    selectMonths: true
-  });
-
-  // Month and Year Select Picker
-  $('.pickadate-months-year').pickadate({
-    selectYears: true,
-    selectMonths: true
-  });
-
-  // Short String Date Picker
-  $('.pickadate-short-string').pickadate({
-    weekdaysShort: ['S', 'M', 'Tu', 'W', 'Th', 'F', 'S'],
-    showMonthsShort: true
-  });
-
-  // Change first weekday
-  $('.pickadate-firstday').pickadate({
-    firstDay: 1
-  });
-
-  /*******    Pick-a-time Picker  *****/
-  // Basic time
-  $('.pickatime').pickatime();
-
-  // Format options
-  $('.pickatime-format').pickatime({
-    // Escape any “rule” characters with an exclamation mark (!).
-    format: 'T!ime selected: h:i a',
-    formatLabel: 'HH:i a',
-    formatSubmit: 'HH:i',
-    hiddenPrefix: 'prefix__',
-    hiddenSuffix: '__suffix'
-  });
-
-  // Format options
-  $('.pickatime-formatlabel').pickatime({
-    formatLabel: function (time) {
-      var hours = (time.pick - this.get('now').pick) / 60,
-        label = hours < 0 ? ' !hours to now' : hours > 0 ? ' !hours from now' : 'now';
-      return 'h:i a <sm!all>' + (hours ? Math.abs(hours) : '') + label + '</sm!all>';
-    }
-  });
-
-  // Min - Max Time to select
-  $('.pickatime-min-max').pickatime({
-    // Using Javascript
-    min: new Date(2015, 3, 20, 7),
-    max: new Date(2015, 7, 14, 18, 30)
-
-    // Using Array
-    // min: [7,30],
-    // max: [14,0]
-  });
-
-  // Intervals
-  $('.pickatime-intervals').pickatime({
-    interval: 150
-  });
-
-  // Disable Time
-  $('.pickatime-disable').pickatime({
-    disable: [
-      // Disable Using Integers
-      3,
-      5,
-      7,
-      13,
-      17,
-      21
-
-      /* Using Array */
-      // [0,30],
-      // [2,0],
-      // [8,30],
-      // [9,0]
-    ]
-  });
 
   // Close on a user action
   $('.pickatime-close-action').pickatime({
-    closeOnSelect: false,
-    closeOnClear: false
+    closeOnSelect: true,
+    closeOnClear: true
   });
 })(window, document, jQuery);
