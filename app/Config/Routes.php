61d39b039c2cc4419f090566e8656363dc75d7e9
Patrick Karungari <pkarungari@gmail.com>
2022-01-03 11:30:31 -0500

    fix

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -37,8 +37,9 @@ $routes->add('dashboard', '\App\Controllers\Admin\Admin::index');
 $routes->add('email', '\App\Controllers\Admin\Email::index');
 $routes->add('chat', '\App\Controllers\Admin\Chat::index');
 $routes->add('users', '\App\Controllers\Admin\Users::index');
+$routes->add('edit/(:any)', '\App\Controllers\Admin\Users::edit/$1');
 $routes->add('view/(:any)', '\App\Controllers\Admin\Users::view/$1');
-
+$routes->add('plans/edit/(:any)', '\App\Controllers\Admin\Plans::edit/$1');
 $routes->add('plans', '\App\Controllers\Admin\Plans::index');
 $routes->add('profile', '\App\Controllers\Admin\Profile::index');
 $routes->add('withdraws', '\App\Controllers\Admin\Withdraws::index');
