1bb13a461304b5a2fdf0372603051ff967746b11
Patrick Karungari <pkarungari@gmail.com>
2022-01-16 06:25:06 -0500

    Update index2.php

--- app/Views/User/Settings/index2.php
+++ app/Views/User/Settings/index2.php
@@ -310,7 +310,7 @@ const phoneInput = window.intlTelInput(phoneInputField, {
     utilsScript: "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js",
 });
 
-$('#phone').val('<?php echo $current_user->phone ?>');
+
 
 
 
